c3f2ddc6f7d1613353724b58732a1e9ea560c306
ahmed3bnby <3bnby93@gmail.com>
2024-07-23 20:22:46 +0300

    first-commit

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Imports;
+
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Collection;
+
+
+class UsersImport implements ToCollection
+{
+    protected $mapping;
+
+    public function __construct(array $mapping)
+    {
+        $this->mapping = $mapping;
+    }
+
+    public function collection(Collection $rows)
+    {
+        $nameKey = array_search(User::NAME_COLUMN, $this->mapping)  ;
+        $phoneKey = array_search(User::PHONE_COLUMN, $this->mapping);
+        $emailKey = array_search(User::EMAIL_COLUMN, $this->mapping);
+        foreach ($rows as $row) {
+            $row = $row->toArray();
+            $data = [
+                'fullname' => isset($row[$nameKey]) ? $row[$nameKey] : null,
+                'phone_number' => isset($row[$phoneKey]) ? (string)$row[$phoneKey] : null,
+                'email' => isset($row[$emailKey]) ? $row[$emailKey] : null,
+            ];
+
+
+            $validator = Validator::make($data, [
+                'fullname' => 'string|max:255',
+                'phone_number' => 'integer',
+                'email' => 'email|max:255',
+            ]);
+
+            if ($validator->fails()) {
+                continue;
+            }
+
+            User::updateOrCreate(
+                ['email' => $data['email']],
+                $data
+            );
+        }
+    }
+}
